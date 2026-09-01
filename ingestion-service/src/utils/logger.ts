3afd4770e11a7f3b4fe40ea994e260502fabd349
Eder Gutierrez <gutierrezeder64@gmail.com>
2026-09-01 15:58:03 -0600

    refactor(logger): restructure logger configuration for worker thread error

--- ingestion-service/src/utils/logger.ts
+++ ingestion-service/src/utils/logger.ts
@@ -1,30 +1,34 @@
 import pino from 'pino';
 
-export const logger = pino({
-    level: process.env.LOG_LEVEL || 'info',
-    transport: {
-        targets: [
-            {
-                target: 'pino-pretty',
-                options: {
-                    colorize: true,
-                    translateTime: 'SYS:standard',
-                    destination: 1
-                }
-            },
-            {
-                target: 'pino-roll',
-                options: {
-                    destination: './logs/ingestion',
-                    mkdir: true,
+const targets: pino.TransportTargetOptions[] = [
+    {
+        target: 'pino-pretty',
+        options: {
+            colorize: true,
+            translateTime: 'SYS:standard',
+            destination: 1
+        }
+    }
+];
 
-                    frequency: 'daily',
-                    extension: '.log',
-                    limit: {
-                        count: 7
-                    }
-                }
+if (process.env.NODE_ENV !== 'test') {
+    targets.push({
+        target: 'pino-roll',
+        options: {
+            file: './logs/ingestion',
+            mkdir: true,
+            frequency: 'daily',
+            extension: '.log',
+            limit: {
+                count: 7
             }
-        ]
+        }
+    });
+}
+
+export const logger = pino({
+    level: process.env.LOG_LEVEL || (process.env.NODE_ENV === 'test' ? 'silent' : 'info'),
+    transport: {
+        targets
     }
 });
